2ee1ca4e38df215728931220e4413803819a76d9
Dave Kwon <davekwon@berkeley.edu>
2019-01-03 21:39:04 -0800

    add GroupSummary test cases

--- src/components/GroupSummary.jsx
+++ src/components/GroupSummary.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GroupSummary = ({ title, creator, numMembers }) => (
-  <div>{title || `${creator} and ${numMembers - 1} more...`}</div>
+const GroupSummary = ({ title, creator, numMembers, updates }) => (
+  <div>
+    <div>{title || `${creator} and ${numMembers - 1} more...`}</div>
+    <div className="chat-icon">{updates === 0 ? '' : updates}</div>
+  </div>
 );
 
 GroupSummary.propTypes = {
   title: PropTypes.string,
   creator: PropTypes.string.isRequired,
-  numMembers: PropTypes.number.isRequired
+  numMembers: PropTypes.number.isRequired,
+  updates: PropTypes.number.isRequired
 };
 
 GroupSummary.defaultProps = {

058c8932241fbd34999c1fd0ccecec800456ac2c
Sungwon Cho <csungwon@berkeley.edu>
2019-01-11 08:56:03 -0800

    add styles to BillDescriptionStep

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import BillDescriptionStep from '../BillDescriptionStep';
 
-const App = () => (
-  <div className="App">
-    <BillDescriptionStep />
-  </div>
-);
+class App extends React.Component {
+  state = {
+    amount: null,
+    description: ''
+  };
+
+  onAmountChange = e => {
+    this.setState({ amount: parseFloat(e.target.value) });
+  };
+
+  onDescriptionChange = e => {
+    this.setState({ description: e.target.value });
+  };
+
+  render() {
+    const { amount, description } = this.state;
+    return (
+      <BillDescriptionStep
+        amount={amount}
+        onAmountChange={this.onAmountChange}
+        description={description}
+        onDescriptionChange={this.onDescriptionChange}
+        onNextButton={() => {
+          console.log('next');
+        }}
+      />
+    );
+  }
+}
 
 export default App;
--- src/components/BillDescriptionStep/index.jsx
+++ src/components/BillDescriptionStep/index.jsx
@@ -2,44 +2,88 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { withStyles } from '@material-ui/core/styles';
+
+import CurrencyFormatter from '../UI/CurrencyFormatter';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    padding: theme.spacing.unit
+  },
+  textField: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
+  },
+  button: {
+    marginTop: 'auto',
+    marginBottom: theme.spacing.unit * 3
+  }
+});
 
 const BillDescriptionStep = ({
   amount,
   description,
   onAmountChange,
   onDescriptionChange,
-  onNextButton
+  onNextButton,
+  classes
 }) => (
-  <div>
+  <div className={classes.container}>
     <TextField
+      className={classes.textField}
       label="Amount"
       variant="outlined"
       placeholder="How much did you pay in total?"
       onChange={onAmountChange}
       value={amount}
+      InputProps={{
+        startAdornment: <InputAdornment position="start">$</InputAdornment>,
+        inputComponent: CurrencyFormatter
+      }}
       fullWidth
+      required
     />
     <TextField
+      className={classes.textField}
       label="Description"
       variant="outlined"
       placeholder="What's it for?"
       onChange={onDescriptionChange}
       value={description}
+      rows={4}
+      rowsMax={20}
+      multiline
       fullWidth
     />
 
-    <Button variant="contained" color="primary" onClick={onNextButton} fullWidth>
+    <Button
+      className={classes.button}
+      variant="contained"
+      color="primary"
+      onClick={onNextButton}
+      size="large"
+      fullWidth
+    >
       Next: Add Friends
     </Button>
   </div>
 );
 
 BillDescriptionStep.propTypes = {
-  amount: PropTypes.number.isRequired,
+  amount: PropTypes.number,
   description: PropTypes.string.isRequired,
   onAmountChange: PropTypes.func.isRequired,
   onDescriptionChange: PropTypes.func.isRequired,
-  onNextButton: PropTypes.func.isRequired
+  onNextButton: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired // eslint-disable-line react/forbid-prop-types
 };
 
-export default BillDescriptionStep;
+BillDescriptionStep.defaultProps = {
+  amount: ''
+};
+
+export default withStyles(styles)(BillDescriptionStep);

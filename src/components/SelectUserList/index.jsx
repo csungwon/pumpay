f0a8af829e0f96af06876502d7675acdfa69b8ff
Sungwon Cho <csungwon@berkeley.edu>
2019-01-12 15:52:40 -0800

    add SelectUserList Component

---
+++ src/components/SelectUserList/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+
+import Avatar from '../UI/Avatar';
+
+const styles = {
+  root: {
+    maxHeight: '70vh',
+    overflowY: 'auto'
+  }
+};
+
+const SelectUserList = ({
+  friends, onSelect, selectedFriends, classes
+}) => (
+  <List classes={{ root: classes.root }}>
+    {friends.map(({ id, imageUrl, username }) => (
+      <ListItem key={id}>
+        <ListItemAvatar>
+          <Avatar imageUrl={imageUrl} />
+        </ListItemAvatar>
+        <ListItemText primary={username} />
+        <ListItemSecondaryAction>
+          <Checkbox onChange={onSelect(id)} checked={selectedFriends.has(id)} />
+        </ListItemSecondaryAction>
+      </ListItem>
+    ))}
+  </List>
+);
+
+SelectUserList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onSelect: PropTypes.func.isRequired,
+  selectedFriends: PropTypes.object.isRequired, // eslint-disable-line
+  classes: PropTypes.object.isRequired // eslint-disable-line
+};
+
+export default withStyles(styles)(SelectUserList);
